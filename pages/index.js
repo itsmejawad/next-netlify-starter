232393050fc18b843e0446014c7b213b49c6de63
itsmejawad <55758370+itsmejawad@users.noreply.github.com>
2022-04-15 05:46:05 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,20 @@ export default function Home() {
       <Head>
         <title>WEB3 for Freedom</title>
         <link rel="icon" href="/favicon.ico" />
+        <style>
+          username {
+    color: linear-gradient(110.78deg, rgb(118, 230, 80) -1.13%, rgb(249, 214, 73) 15.22%, rgb(240, 142, 53) 32.09%, rgb(236, 81, 87) 48.96%, rgb(255, 24, 189) 67.94%, rgb(26, 75, 255) 85.34%, rgb(98, 216, 249) 99.57%) text;
+    background: linear-gradient(110.78deg, rgb(118, 230, 80) -1.13%, rgb(249, 214, 73) 15.22%, rgb(240, 142, 53) 32.09%, rgb(236, 81, 87) 48.96%, rgb(255, 24, 189) 67.94%, rgb(26, 75, 255) 85.34%, rgb(98, 216, 249) 99.57%) text;
+}
+        </style>
       </Head>
 
       <main>
         <Header>
-        <h1 class"title"> Follow us @WEB3forFreedom <h1/>
+        <h1 class"title"> Follow us <username> @WEB3forFreedom </username> <h1/>
         <p className="description">
           Get started by editing <code>pages/index.js</code
-        <Header/>
+        </Header>
         </p>
       </main>
 
